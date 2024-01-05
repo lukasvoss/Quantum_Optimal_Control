a082409cb33d33583b48e59d3fbd280a8a657772
lukasvoss <lukas_voss@icloud.com>
2024-01-05 18:50:27 +0800

    first attempt hybrid job, execution failed; need to check the logs for the reason

--- 00_AWS/04_Tests/runs/test/00_Hybrid_Job/algorithm_script.py
+++ 00_AWS/04_Tests/runs/test/00_Hybrid_Job/algorithm_script.py
@@ -1,18 +1,21 @@
-import os
-
 from braket.aws import AwsDevice
 from braket.circuits import Circuit
+from braket.jobs import save_job_result #ADD
 
 def start_here():
 
     print("Test job started!!!!!")
 
-    # Use the device declared in the job script
-    device = AwsDevice(os.environ["AMZN_BRAKET_DEVICE_ARN"])
+    device = AwsDevice(os.environ['AMZN_BRAKET_DEVICE_ARN'])
+
+    results = []  #ADD
 
     bell = Circuit().h(0).cnot(0, 1)
     for count in range(5):
         task = device.run(bell, shots=100)
         print(task.result().measurement_counts)
+        results.append(task.result().measurement_counts)  #ADD
+
+        save_job_result({ "measurement_counts": results })  #ADD
 
     print("Test job completed!!!!!")
