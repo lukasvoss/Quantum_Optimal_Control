dbcb6f6f0b3fd1c8d66340a08b166026300f5657
Lukas Voss <lukas_voss@icloud.com>
2025-01-29 13:54:27 +0800

    First working version of running the RL training in a script
    ToDo
    - Improve parsing of custom parameters for the RL training
    - Save results on disc

--- gate_level/spillover_noise_use_case/generic_spillover/run_effect_on_subsystem.py
+++ gate_level/spillover_noise_use_case/generic_spillover/run_effect_on_subsystem.py
@@ -1,147 +1,58 @@
-# %%
 import sys
-import os
 project_path = '/Users/lukasvoss/Documents/Master Wirtschaftsphysik/Masterarbeit Yale-NUS CQT/Quantum_Optimal_Control/'
 sys.path.append(project_path)
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from qiskit import QuantumRegister
+from qiskit.circuit import QuantumCircuit, ParameterVector
+from qiskit.transpiler import PassManager, CouplingMap
+from qiskit.qasm3 import dumps as qasm3_dumps
+from gymnasium.spaces import Box
+
+# Custom imports from project
 from gate_level.spillover_noise_use_case.generic_spillover.spillover_effect_on_subsystem import (
     LocalSpilloverNoiseAerPass,
     circuit_context,
     numpy_to_hashable,
     noisy_backend,
 )
-from qiskit.circuit import QuantumCircuit, ParameterVector
-from qiskit.transpiler import PassManager, CouplingMap
-import numpy as np
-import matplotlib.pyplot as plt
-
-num_qubits = 6
-rotation_axes = ["rx", "rx", "rx", "rx", "rx", "rx"]
-rotation_parameters = ParameterVector("θ", num_qubits)
-cm = CouplingMap.from_line(num_qubits, True)
-print("Coupling map:")
-cm.draw()
-
-# %%
-circuit = circuit_context(
-    num_qubits, rotation_axes, rotation_parameters, coupling_map=cm
+from rl_qoc import (
+    QEnvConfig,
+    ExecutionConfig,
+    ContextAwareQuantumEnvironment,
+    RescaleAndClipAction,
 )
-circuit.draw("mpl")
-
-# %%
-seed = 42
-np.random.seed(seed)
-rotation_angles = np.random.uniform(0, 2 * np.pi, num_qubits)
-# rotation_angles = np.array([0.0, 0.0, np.pi, 0., 0.0, 0.0])
-param_dict = {theta: val for theta, val in zip(rotation_parameters, rotation_angles)}
-print("Parameter dictionary:\n", param_dict)
-circuit.assign_parameters(param_dict).draw("mpl")
-
-# %%
-# Define noise-coupling matrix with gamma values (example values)
-gamma_matrix = np.zeros((num_qubits, num_qubits))
-gamma_matrix[0, 1] = 0.000  # γ_0->1
-gamma_matrix[0, 3] = 0.0  # γ_0->3
-gamma_matrix[1, 2] = 0.0  # γ_1->2
-gamma_matrix[2, 2] = 0.1
-gamma_matrix[2, 3] = 0.04  # γ_2->3
-gamma_matrix[3, 2] = 0.0  # γ_3->2
-gamma_matrix[4, 3] = 0.0  # γ_4->3
-gamma_matrix[5, 4] = 0.0  # γ_5->4
-gamma_matrix = 0.05 * np.round(np.random.rand(num_qubits, num_qubits), 3)
-# Display gamma matrix with imshow plot for better visualization
-plt.imshow(gamma_matrix, cmap="hot", interpolation="nearest")
-plt.colorbar()
-plt.title("Spillover rate matrix")
-
-# Display gamma matrix as weights in a directed graph
-# import networkx as nx
-#
-# G = nx.DiGraph(gamma_matrix)
-# pos = nx.spring_layout(G)
-# nx.draw(G, None, with_labels=True, node_size=1000, node_color="skyblue", font_size=10)
-# labels = nx.get_edge_attributes(G, "weight")
-# nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-# plt.title("Spillover rate matrix as directed graph")
-
-# %%
-gamma_matrix
-
-# %%
-pm = PassManager(
-    [
-        LocalSpilloverNoiseAerPass(
-            spillover_rate_matrix=numpy_to_hashable(gamma_matrix),
-            target_subsystem=(2, 3),
-        )
-    ]
-)
-qc_noisy = pm.run(circuit.assign_parameters(param_dict))
-qc_noisy.draw("mpl")
-
-# %%
-backend = noisy_backend(
-    circuit.assign_parameters(param_dict),
-    gamma_matrix,
-    target_subsystem=(2, 3),
-)
-
-# %%
-backend.operation_names
-
-# %%
-print("Rotation angles:", rotation_angles)
-
-# %%
-backend.options.noise_model.to_dict()
-
-# %%
-# from qiskit.quantum_info import Operator
-
-# Operator(qc_noisy)
-
-# # %%
-# from qiskit.providers.basic_provider import BasicSimulator
-# from qiskit_aer import AerSimulator
-# from qiskit.visualization import plot_histogram
-
-# ideal_backend = AerSimulator()
-
-# ideal_result = ideal_backend.run(qc_noisy.measure_all(inplace=False)).result()
-# noisy_result = backend.run(qc_noisy.measure_all(inplace=False)).result()
-
-# ideal_counts = ideal_result.get_counts()
-# noisy_counts = noisy_result.get_counts()
-
-# plot_histogram([ideal_counts, noisy_counts], legend=["Ideal", "Noisy"])
-
-# %%
-# Compute fidelities
-# from qiskit.quantum_info import state_fidelity, average_gate_fidelity
-
-# qc_noisy_ideal_state = qc_noisy.copy()
-# qc_noisy_state = qc_noisy.copy()
-# # qc_noisy_ideal_state.save_statevector()
-# qc_noisy_ideal_state.save_unitary()
-# # qc_noisy_state.save_statevector()
-# qc_noisy_state.save_unitary()
-
-# # ideal_state = ideal_backend.run(qc_noisy_ideal_state).result().get_statevector()
-# # noisy_state = backend.run(qc_noisy_state).result().get_statevector()
-# ideal_unitary = ideal_backend.run(qc_noisy_ideal_state).result().get_unitary()
-# noisy_unitary = backend.run(qc_noisy_state).result().get_unitary()
-
-# # %%
-# output_fidelity = average_gate_fidelity(ideal_unitary, noisy_unitary)
-# output_fidelity
-
-# %%
-from qiskit import QuantumRegister
+from rl_qoc.environment.configuration.backend_config import QiskitConfig
 from rl_qoc.helpers.circuit_utils import get_gate
+from rl_qoc.agent import (
+    PPOConfig, 
+    CustomPPO, 
+    TrainingConfig, 
+    TrainFunctionSettings, 
+    TotalUpdates
+)
 
+def parse_arguments():
+    """Parse command-line arguments for script customization."""
+    parser = argparse.ArgumentParser(description="Train RL agent for quantum gate calibration.")
+    parser.add_argument("--num_qubits", type=int, default=6, help="Number of qubits in the circuit.")
+    parser.add_argument("--target_gate", type=str, default="cnot", help="Type of gate to calibrate.")
+    parser.add_argument("--gamma_scale", type=float, default=0.05, help="Scaling factor for spillover noise.")
+    parser.add_argument("--total_updates", type=int, default=500, help="Total updates for RL training.")
+    parser.add_argument("--seed", type=int, default=42, help="Random seed.")
+    return parser.parse_args()
+
+def setup_quantum_circuit(num_qubits, seed):
+    """Initialize quantum circuit with parameterized rotations."""
+    np.random.seed(seed)
+    rotation_axes = ["rx"] * num_qubits
+    rotation_parameters = ParameterVector("θ", num_qubits)
+    cm = CouplingMap.from_line(num_qubits, bidirectional=True)
+    circuit = circuit_context(num_qubits, rotation_axes, rotation_parameters, coupling_map=cm)
+    return circuit, rotation_parameters, cm
 
 # Add here custom ansatz CX gate
-
-
 def apply_parametrized_circuit(
     qc: QuantumCircuit, params: ParameterVector, q_reg: QuantumRegister, **kwargs
 ):
@@ -172,221 +83,113 @@ def apply_parametrized_circuit(
 
     qc.append(my_qc.to_instruction(label=my_qc.name), q_reg)
 
-# %%
-# Define the environment
-from gymnasium.spaces import Box
-from rl_qoc import (
-    QEnvConfig,
-    ExecutionConfig,
-    ContextAwareQuantumEnvironment,
-    RescaleAndClipAction,
-)
-from rl_qoc.environment.configuration.backend_config import QiskitConfig
+def generate_noise_matrix(num_qubits, scale):
+    """Generate a spillover noise matrix."""
+    gamma_matrix = scale * np.round(np.random.rand(num_qubits, num_qubits), 3)
+    return gamma_matrix
 
-env_metadata = {
-    "γ": gamma_matrix,
-    "target_subsystem": (2, 3),
-    "rotation_axes": rotation_axes,
-    "num_qubits": num_qubits,
-    "rotation_parameters": rotation_parameters,
-    "seed": seed,
-}
-action_space = Box(low=-1.0, high=1.0, shape=(7,), dtype=np.float32)
-target = {"gate": "cnot", "physical_qubits": [0, 1]}
+def apply_noise_pass(circuit, gamma_matrix, param_dict):
+    """Apply custom noise pass to the circuit."""
+    pm = PassManager(
+            [LocalSpilloverNoiseAerPass(
+                spillover_rate_matrix=numpy_to_hashable(gamma_matrix), target_subsystem=(2, 3)
+            )]
+        )
+    return pm.run(circuit.assign_parameters(param_dict))
 
-backend_config = QiskitConfig(
-    apply_parametrized_circuit,
-    backend=backend,
-    skip_transpilation=False,
-    parametrized_circuit_kwargs={"target": target, "backend": backend},
-    pass_manager=None,
-)
-q_env_config = QEnvConfig(
-    backend_config=backend_config,
-    target=target,
-    action_space=action_space,
-    execution_config=ExecutionConfig(
-        batch_size=32, n_reps=[4, 7, 9, 12], n_shots=100, sampling_paulis=40, c_factor=1
-    ),
-    reward_config="cafe",
-    env_metadata=env_metadata,
-)
-q_env = ContextAwareQuantumEnvironment(
-    q_env_config,
-    circuit_context=qc_noisy,  # Pass the circuit already transpiled through custom pass
-)
-
-# %%
-rescaled_env = RescaleAndClipAction(q_env, -1, 1)
-
-# %%
-q_env.backend_info.custom_transpile(q_env.circuits[0]).draw("mpl")
-
-# %%
-from rl_qoc.agent import PPOConfig
-
-agent_config = PPOConfig.from_yaml(os.path.join(project_path, "gate_level/spillover_noise_use_case/agent_config.yaml"))
-print(agent_config)
-
-# %%
-from rl_qoc.agent import CustomPPO, TrainingConfig, TrainFunctionSettings, TotalUpdates
-
-ppo_agent = CustomPPO(agent_config, rescaled_env, save_data=False)
-total_updates = 500
-target_fidelities = [0.999]
-lookback_window = 20
-anneal_learning_rate = True
-
-ppo_config = TrainingConfig(
-    TotalUpdates(total_updates),
-    target_fidelities,
-    lookback_window,
-    anneal_learning_rate,
-)
-train_settings = TrainFunctionSettings(
-    plot_real_time=True,
-    print_debug=False,
-    num_prints=10,
-    hpo_mode=False,
-    clear_history=True,
-)
-
-# %%
-# %matplotlib inline
-ppo_agent.train(ppo_config, train_settings)
-
-# %%
-q_env.pubs[10].circuit.draw("mpl")
-
-# %%
-from qiskit.qasm3 import dumps as qasm3_dumps
-
-print(
-    qasm3_dumps(
-        circuit_context(num_qubits, rotation_axes, rotation_parameters, coupling_map=cm)
+def define_backend(circuit, gamma_matrix, param_dict):
+    """Define the backend with noise model."""
+    return noisy_backend(
+        circuit.assign_parameters(param_dict), 
+        gamma_matrix, 
+        target_subsystem=(2, 3)
     )
-)
 
-# %%
-# Plotting the learning curve
-import matplotlib.pyplot as plt
-
-reward_history = np.array(q_env.reward_history)
-mean_rewards = np.mean(reward_history, axis=-1)
-max_mean = np.round(np.max(mean_rewards), 4)
-arg_max_mean = np.argmax(mean_rewards)
-baseline_fidelity = np.round(
-    q_env.simulate_circuit(
-        q_env.circuits[0],
-        np.zeros((1, q_env.action_space.shape[-1])),
-        update_env_history=False,
-    ),
-    4,
-)[0]
-
-plot_reward = True
-plot_fidelity = True
-plot_circuit_fidelity = False
-plot_fidelity_nreps = False
-plot_circuit_fidelity_nreps = False
-
-if plot_reward:
-    plt.plot(mean_rewards, label=f"Reward, max: {max_mean}")
-if plot_fidelity:
-    plt.plot(
-        q_env.fidelity_history,
-        label=f"Avg Fidelity, max: {np.round(np.max(q_env.fidelity_history),4)}",
+def train_rl_agent(env, total_updates):
+    """Train RL agent using PPO algorithm."""
+    agent_config = PPOConfig.from_yaml("gate_level/spillover_noise_use_case/agent_config.yaml")
+    ppo_agent = CustomPPO(agent_config, env, save_data=False)
+    
+    ppo_config = TrainingConfig(
+        TotalUpdates(total_updates),
+        target_fidelities=[0.999],
+        lookback_window=20,
+        anneal_learning_rate=True,
     )
-if plot_circuit_fidelity_nreps:
-    plt.plot(
-        q_env.circuit_fidelity_history_nreps,
-        label=f"Circuit Fidelity nreps(={q_env.n_reps})",
+    train_settings = TrainFunctionSettings(
+        plot_real_time=True,
+        print_debug=False,
+        num_prints=10,
+        hpo_mode=False,
+        clear_history=True,
     )
-if plot_fidelity_nreps:
-    plt.plot(
-        q_env.avg_fidelity_history_nreps, label=f"Avg Fidelity nreps(={q_env.n_reps})"
+    ppo_agent.train(ppo_config, train_settings)
+
+def plot_learning_curve(env):
+    """Plot RL agent learning curve."""
+    reward_history = np.array(env.reward_history)
+    mean_rewards = np.mean(reward_history, axis=-1)
+    plt.plot(mean_rewards, label="Mean Batch Rewards")
+    plt.plot(env.fidelity_history, label="Fidelity")
+    plt.legend()
+    plt.show()
+
+def main():
+    args = parse_arguments()
+    
+    # Setup quantum circuit
+    circuit, rotation_parameters, cm = setup_quantum_circuit(args.num_qubits, args.seed)
+    gamma_matrix = generate_noise_matrix(args.num_qubits, args.gamma_scale)
+    
+    rotation_angles = np.random.uniform(0, 2 * np.pi, args.num_qubits)
+    param_dict = {theta: val for theta, val in zip(rotation_parameters, rotation_angles)}
+    
+    noisy_circuit = apply_noise_pass(circuit, gamma_matrix, param_dict)
+    backend = define_backend(circuit, gamma_matrix, param_dict)
+    
+    # Define the RL training environment
+    action_space = Box(low=-1.0, high=1.0, shape=(7,), dtype=np.float32)
+    target = {"gate": args.target_gate, "physical_qubits": [0, 1]}
+    backend_config = QiskitConfig(
+        apply_parametrized_circuit,
+        backend=backend,
+        skip_transpilation=False,
+        parametrized_circuit_kwargs={"target": target, "backend": backend},
+        pass_manager=None,
     )
-if plot_circuit_fidelity:
-    plt.plot(q_env.circuit_fidelity_history, label="Circuit Fidelity")
-plt.axhline(
-    baseline_fidelity,
-    label=f"Baseline Fidelity ({baseline_fidelity})",
-    color="red",
-    linestyle="--",
-)
-plt.axhline(
-    q_env.simulate_circuit(
-        q_env.circuits[0], np.array([q_env.optimal_action]), update_env_history=False
-    ),
-    label=f"Optimal Fidelity = {q_env.fidelity_history[arg_max_mean]}(by max reward, reached at step {arg_max_mean})",
-    color="green",
-    linestyle="--",
-)
-plt.axhline(
-    np.max(q_env.fidelity_history),
-    label=f"Max Avg Fidelity ={np.max(q_env.fidelity_history)}(reached at step {np.argmax(q_env.fidelity_history)})",
-    color="purple",
-    linestyle="--",
-)
-plt.xlabel("Updates")
-plt.ylabel("Reward")
-# plt.title(
-#     f"{q_env.target.gate.name.upper()} Learning Curve, method: {q_env.config.reward_method},"
-#     f" backend: {q_env.config.backend.name if q_env.config.backend is not None else 'No backend'}"
-# )
-plt.ylim(0.99, 1.00001)
-plt.yscale("log")
-plt.legend()
+    
+    q_env_config = QEnvConfig(
+        backend_config=backend_config,
+        target=target,
+        action_space=action_space,
+        execution_config=ExecutionConfig(
+            batch_size=32, n_reps=[4, 7, 9, 12], n_shots=100, sampling_paulis=40, c_factor=1
+        ),
+        reward_config="cafe",
+        env_metadata={
+            "γ": gamma_matrix,
+            "target_subsystem": (2, 3),
+            "rotation_axes": ["rx"] * args.num_qubits,
+            "num_qubits": args.num_qubits,
+            "rotation_parameters": rotation_parameters,
+            "seed": args.seed,
+        },
+    )
+    
+    q_env = ContextAwareQuantumEnvironment(
+        q_env_config, 
+        circuit_context=noisy_circuit
+    )
+    rescaled_env = RescaleAndClipAction(q_env, -1, 1)
+    
+    # Train RL agent
+    train_rl_agent(rescaled_env, args.total_updates)
+    
+    # Plot learning curve
+    # plot_learning_curve(q_env)
+    
+    # Output final circuit
+    print(qasm3_dumps(q_env.pubs[6].circuit))
 
-# %%
-from qiskit.qasm3 import dumps as qasm3_dumps
-
-circuit = q_env.pubs[6].circuit
-circuit.draw("mpl")
-
-# %%
-print(qasm3_dumps(circuit))
-
-# %%
-q_env._pauli_shots
-
-# %%
-len(np.nonzero(np.round(q_env.target.Chi, 5))[0])
-
-# %%
-# Plotting the learning curve
-
-reward_history = np.array(q_env.reward_history)
-mean_rewards = np.mean(reward_history, axis=-1)
-max_mean = int(np.max(mean_rewards) * 1e4) / 1e4
-
-plt.plot(mean_rewards, label=f"Mean Batch Rewards, max: {max_mean}")
-plt.plot(
-    q_env.fidelity_history,
-    label=f"Fidelity, max: {int(np.max(q_env.fidelity_history) * 1e4) / 1e4}",
-)
-plt.plot(q_env.circuit_fidelity_history_nreps, label="Circuit Fidelity nreps")
-plt.plot(q_env.avg_fidelity_history_nreps, label="Avg Fidelity nreps")
-plt.plot(q_env.circuit_fidelity_history, label="Circuit Fidelity")
-plt.xlabel("Updates")
-plt.ylabel("Reward")
-plt.title(
-    f"{q_env.target.gate.name.upper()} Learning Curve, method: {q_env.config.reward_method},"
-    f" backend: {q_env_config.backend.name if q_env_config.backend is not None else 'No backend'}"
-)
-plt.legend()
-plt.show()
-
-# %%
-q_env.optimal_action
-
-# %%
-q_env.optimal_action
-
-# %%
-q_env.backend.operation_names
-
-# %%
-
-
-
+if __name__ == "__main__":
+    main()
